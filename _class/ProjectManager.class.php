88f011b983584f98b150b6deadabadab5102e109
Michel Buhendwa <MTazy@EL.Taz>
2017-10-07 16:35:07 +0300

    tes

---
+++ _class/ProjectManager.class.php
@@ -0,0 +1,35 @@
+<?php
+////////////////script start
+include_once 'project.class.php'
+
+class ProjectManager{
+	private $_db;
+	public function __construct($db){
+		$this->setDb($db);
+	}
+	public function add(Project $perso){
+
+	}
+	public function delete(Project $perso){
+
+	}
+	public function get($id){
+	// Returns one Project object from its id.
+
+	}
+	public function getList(){
+	// Returns a list of projects.
+	}
+	public function update(Project $proj){
+	// Prépare une requête de type UPDATE.
+	// Assignation des valeurs à la requête.
+	// Exécution de la requête.
+	}
+	//setter
+	public function setDb(PDO $db){
+		$this->_db = $db;
+	}
+}
+
+////////////////script end
+?>
