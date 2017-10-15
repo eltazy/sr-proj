49dd5cb93d92939e056819e1aa5f14fe08f0c781
Michel Buhendwa <mkbuhendwa@gmail.com>
2017-10-15 19:10:19 +0300

    week 9

--- index.php
+++ index.php
@@ -1,19 +1,9 @@
 <?php
     if (!isset($_SESSION)) session_start();
+	
 	include_once("_pages/header.php");
 ?>
-<!DOCTYPE html>
-<html>
 <head>
 	<title>Home</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
-	<body>
-		Sign up now!
-		<form method="post" action="signup.php">
-			<input type="submit" name="type" value="Student">
-			<input type="submit" name="type" value="Lecturer">
-		</form>
-		or <a href="login.php">Log in!</a>
-	</body>
-</html>
--- profile.php
+++ profile.php
@@ -1,5 +1,6 @@
 <?php
 	if(!isset($_SESSION)) session_start();
+	
 	include '_pages/header.php';
 	
 	if(isset($_GET['change'])){
